a7ff95ed930fa4db0d4a91fad62b6aee1b2eb2d4
AceMouse <humle000@gmail.com>
2024-02-08 17:09:30 +0100

    add iterative procedual version

---
+++ week1/sir_iterative_procedual.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+
+double I = 1;          // Infected
+double N = 1000;       // Population
+double S = N - I;      // Suspectible but not yet infected
+double R = N - S - I;  // Recovered and now immune
+
+double infection_rate = 1./5.;  // Beta
+double recovery_rate = 1./10.;  // Gamma
+
+double step_size = .5;
+double steps = 10000;
+
+int main (int argc, char *argv[]) {
+    FILE *outFile;
+    outFile = fopen("sir_output.txt", "w");
+
+    fprintf(outFile, "S I R\n");
+    double is; 
+    double ir; 
+    double frac = 1/N; //save on divisions
+    while(steps-- && I>=1){
+        fprintf(outFile, "%.3f %.3f %.3f\n", S, I, R);
+        is = infection_rate * I * S * frac;
+        ir = recovery_rate * I;
+        S -= is * step_size;
+        I += (is - ir) * step_size;
+        R += ir * step_size;
+    }
+    fclose(outFile);
+    return 0;
+}
