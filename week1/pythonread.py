69785c75a80875f5c294de3417e92ab502028dae
idarye <123239382+idarye@users.noreply.github.com>
2024-02-09 12:27:25 +0100

    adjust plotting x-axis

--- week1/pythonread.py
+++ week1/pythonread.py
@@ -1,9 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 x, y, z = np.loadtxt('sir_output.txt', skiprows=1, unpack=True)
-plt.plot(x, label = "S")
-plt.plot(y, label = "I")
-plt.plot(z, label = "R")
+
+step_size = 0.5
+days = np.arange(0, len(x)*step_size, step_size)
+
+plt.plot(days, x, label = "S")
+plt.plot(days, y, label = "I")
+plt.plot(days, z, label = "R")
 plt.title("SIR model")
 plt.ylabel("Individuals")
 plt.xlabel("Days (since first infection)")
